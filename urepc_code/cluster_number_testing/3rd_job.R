c543567a3891041d731db4426396476d8788bf3a
Gabriel Lozano <glozanop@purdue.edu>
2021-09-22 10:43:31 -0400

    cluster selection done

--- urepc_code/cluster_number_testing/3rd_job.R
+++ urepc_code/cluster_number_testing/3rd_job.R
@@ -3,20 +3,20 @@ library("mixtools")
 data = read.csv(
   "./data/1_5_CPMcutoff_suffix_1_log_cero_replacement.csv",
   header = TRUE
-  )
+)
 
 data <- data.frame(lapply(data[,-1], as.numeric), row.names = data[,1])
 
 start_time <- Sys.time()
-mix <- npEM(data, 5 , samebw = FALSE ,verb = TRUE,maxiter = 500)
+mix <- npEM(data, 8 , samebw = FALSE ,verb = TRUE,maxiter = 500)
 end_time <- Sys.time()
 
 print(end_time-start_time)
 
 saveRDS(mix,
-        file = "./results/np_models/npEM_k_5_1_5_CPMcutoff_suffix_1_log_cero_replacement.RDS")
+        file = "./results/np_models/npEM_k_8_1_5_CPMcutoff_suffix_1_log_cero_replacement.RDS")
 
 write.csv(
   mix[["posteriors"]],
-  "./results/np_posteriors/npEM_k_5_1_5_CPMcutoff_suffix_1_log_cero_replacement_posteriors.csv",
+  "./results/np_posteriors/npEM_k_8_1_5_CPMcutoff_suffix_1_log_cero_replacement_posteriors.csv",
 )
