2d462f09bf6fd4deead8203eb58a8e2ac64685dd
Gabriel Octavio Lozano Pinzon <glozanop@bell-a012.rcac.purdue.edu>
2021-09-24 14:53:37 -0400

    Finished running long cluster selection model

--- urepc_code/cluster_number_testing/select_clusters_individual.R
+++ urepc_code/cluster_number_testing/select_clusters_individual.R
@@ -1,18 +1,25 @@
 library("mixtools")
 
-name = "./data/1_5_CPMcutoff_suffix_1_log_cero_replacement.csv"
+name = "./urepc/urepc_code/cluster_number_testing/data/1_5_CPMcutoff_suffix_1_log_cero_replacement.csv"
 max_selection = 10
 
 #read the data
 data <- read.csv(name)[,c(2,3,4,5)]
 data <- as.matrix(data)
 #perform the selection
-cluster_selection <- repnormmixmodel.sel(
+start_time <- Sys.time()
+cluster_selection <- repnormmixmodel.sel( 
   t(data), 
   k = max_selection, 
   verb = TRUE, 
-  epsilon = 1e-03
+  epsilon = 1e-08,
+  maxit = 20000
 )
+end_time <- Sys.time()
 
-saveRDS(cluster_selection,file = "./results/cluster_sel_1_5_CPMcutoff_suffix_1_log_cero_replacement_k_10.RDS")
+print(end_time-start_time)
+
+
+saveRDS(cluster_selection,file = "./urepc/urepc_code/cluster_number_testing/results/cluster_sel_LONG_1_5_CPMcutoff_suffix_1_log_cero_replacement_k_10.RDS")
+saveRDS(cluster_selection,file = "./results/cluster_sel_LONG_1_5_CPMcutoff_suffix_1_log_cero_replacement_k_10.RDS")
 #foo = readRDS("./results/cluster_sel_2_0_CPMcutoff_suffix_1_log_cero_replacement_k_10.RDS")
