40fc7674897a46165e905b50271a4250b3670253
Gabriel Lozano <glozanop@purdue.edu>
2021-09-24 16:29:24 -0400

    Added code to install poisson libraries

--- urepc_code/poisson_modeling/replicate_results.R
+++ urepc_code/poisson_modeling/replicate_results.R
@@ -1,9 +1,19 @@
 # Replicate results from Co-expression analysis of RNA-seq data with 
 # the HTSClusterpackage
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("edgeR")
+BiocManager::install("HTSFilter")
+BiocManager::install("HTSCluster")
+BiocManager::install("Biobase", force = TRUE)
+
+
 library(HTSCluster)
 library(HTSFilter)
 library(Biobase)
 data(sultan)
 conds <- as.vector(phenoData(sultan)$cell.line)
 y <- exprs(sultan)
+
