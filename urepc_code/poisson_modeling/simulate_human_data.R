5d135b5902687193ff7b79558c4fafe83c51656e
Gabriel Lozano <glozanop@purdue.edu>
2021-11-01 14:38:16 -0400

    fixes simulated data column alignment
    fixes to lambda plots that were poorly mande.

--- urepc_code/poisson_modeling/simulate_human_data.R
+++ urepc_code/poisson_modeling/simulate_human_data.R
@@ -11,32 +11,6 @@ data_filtered <- y.filter$filteredData
 
 first_poisson_15 <- PoisMixClus(data.matrix(data_filtered), 15, norm = "TMM",   conds=conds)
 
-# set.seed(12345)
-# #run the clustering
-# start_time <- Sys.time()
-# PMM <- PoisMixClusWrapper(
-#   y= data.matrix(data_filtered) ,
-#   gmin=1,
-#   gmax=35,
-#   conds=conds,
-#   split.init=TRUE,
-#   norm="TMM"
-# )
-# 
-# end_time <- Sys.time()
-# 
-# end_time - start_time
-# 
-# saveRDS(PMM, file = "./human_sim_data/poisson_mixture_model.RDS")
-# write.csv(data_filtered, file = "./human_sim_data/data_human.csv")
-# 
-# data_points <- data_filtered
-# normalization <- PMM$Djump.results$norm
-# lambda <- PMM$Djump.results$lambda
-# labels <- PMM$Djump.results$labels
-# pi <- PMM$Djump.results$pi
-
-
 data_points <- data_filtered
 normalization <- first_poisson_15$norm
 lambda <- first_poisson_15$lambda
@@ -46,14 +20,20 @@ pi <- first_poisson_15$pi
 
 
 w <- rowSums(data.matrix(data_points))
-s <- normalization
 labels <- as.numeric(unlist(labels))
 
 cluster_size <- length(pi)
 
+normalization = c()
 lambda_per_cluster <- list()
 for (k in seq(cluster_size)){
-  lambda_per_cluster[[k]] <- outer(as.numeric(w), as.vector(unlist(lambda[,k]*normalization)), '*')
+  lambda_aux <- c(
+    lambda[,k][['HEK293T']],
+    lambda[,k][['HEK293T']],
+    lambda[,k][['Ramos B cell']],
+    lambda[,k][['Ramos B cell']]
+  )
+  lambda_per_cluster[[k]] <- outer(as.numeric(w), as.vector(unlist(lambda_aux*normalization)), '*')
 }
 
 sim_data <- matrix(, nrow = length(w), ncol = length(normalization))
@@ -61,14 +41,14 @@ for (row in seq(length(w))){
   sim_data[row,] <- rpois(length(normalization),lambda_per_cluster[[labels[row]]][row,])
 }
 
-write.csv(sim_data, file = "./human_sim_data/sim_data_1.csv")
-write.csv(labels, file = "./human_sim_data/sim_data_1_labels.csv")
+write.csv(sim_data, file = "./human_sim_data/sim_data_2.csv")
+write.csv(labels, file = "./human_sim_data/sim_data_2_labels.csv")
 
 
 start_time <- Sys.time()
-mix_pois_sim <- PoisMixClus(data.matrix(sim_data), 15, norm = "TMM",   conds=c("a","b","a","b"))
+mix_pois_sim <- PoisMixClus(data.matrix(sim_data), 15, norm = "TMM",   conds=c("a","a","b","b"))
 end_time <- Sys.time()
 
 end_time - start_time
 
-write.csv(mix_pois_sim$labels, file = "./human_sim_data/sim_data_poisson_clustering_14.csv")
+write.csv(mix_pois_sim$labels, file = "./human_sim_data/sim_data_2_poisson_clustering_14.csv")
